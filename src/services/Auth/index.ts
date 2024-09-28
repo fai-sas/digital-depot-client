8dfcb6170a94c031d201f897146a4c4bd58e72d1
fai-sas <craftedbyfaisal@gmail.com>
2024-09-28 21:55:43 +0600

    auth service added

---
+++ src/services/Auth/index.ts
@@ -0,0 +1,83 @@
+'use server'
+
+import { FieldValues } from 'react-hook-form'
+import { cookies } from 'next/headers'
+import { jwtDecode } from 'jwt-decode'
+
+import axiosInstance from '@/src/lib/AxiosInstance'
+
+export const registerUser = async (userData: FieldValues) => {
+  try {
+    const { data } = await axiosInstance.post('/auth/register', userData)
+
+    if (data.success) {
+      cookies().set('accessToken', data?.data?.accessToken)
+      cookies().set('refreshToken', data?.data?.refreshToken)
+    }
+
+    return data
+  } catch (error: any) {
+    throw new Error(error)
+  }
+}
+
+export const loginUser = async (userData: FieldValues) => {
+  try {
+    const { data } = await axiosInstance.post('/auth/login', userData)
+
+    if (data.success) {
+      cookies().set('accessToken', data?.data?.accessToken)
+      cookies().set('refreshToken', data?.data?.refreshToken)
+    }
+
+    return data
+  } catch (error: any) {
+    throw new Error(error)
+  }
+}
+
+export const logout = async () => {
+  cookies().delete('accessToken')
+  cookies().delete('refreshToken')
+}
+
+export const getCurrentUser = async () => {
+  const accessToken = cookies().get('accessToken')?.value
+
+  let decodedToken = null
+
+  if (accessToken) {
+    decodedToken = await jwtDecode(accessToken)
+
+    return {
+      _id: decodedToken.userId,
+      name: decodedToken.name,
+      email: decodedToken.email,
+      mobileNumber: decodedToken.mobileNumber,
+      role: decodedToken.role,
+      status: decodedToken.status,
+      profilePhoto: decodedToken.profilePhoto,
+    }
+  }
+
+  return decodedToken
+}
+
+export const getNewAccessToken = async () => {
+  try {
+    const refreshToken = cookies().get('refreshToken')?.value
+
+    const res = await axiosInstance({
+      url: '/auth/refresh-token',
+      method: 'POST',
+      withCredentials: true,
+      headers: {
+        cookies: `refreshToken=${refreshToken}`,
+      },
+    })
+
+    return res.data
+  } catch (error) {
+    throw new Error('Failed to get new access token')
+  }
+}
